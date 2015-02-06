9dc598233cf045b720c5c11b23366f229df166d7
bpr000@post.uit.no <bpr000@ifilab65.stud.cs.uit.no>
2015-02-06 18:10:22 +0100

    Registration form with password encryption done

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,18 +3,27 @@ class AccountsController < ApplicationController
 		@account = Account.new
 	end
 	
+	def show
+	end
+	
+	def edit
+	end
+	
 	def create
 		@account = Account.new(account_params)
 		if @account.save
 			flash[:notice] = "Account successfully created."
-			redirect_to 
+			redirect_to @account
 		else
+		  render :new
 		end
 	end
+	
+	
 	
 	private
 	
 	def account_params
-		params.require(:account).permit(:username, :email, :password)
+		params.require(:account).permit(:username, :email, :password, :password_confirmation)
 	end
 end

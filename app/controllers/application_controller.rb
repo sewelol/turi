8340dbbce40654d771dbaf36374c239042caba45
Florian Sauter <fn.sauter@gmail.com>
2015-02-16 17:35:45 +0100

    Custom theme improvements.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
-def require_signin!
+  def require_signin!
         if current_user.nil?
             flash[:error] = I18n.t 'sign_in_to_create_trip'
             redirect_to sign_in_url

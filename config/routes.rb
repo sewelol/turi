5d73cfb561ddaea05c3a8ede12a16b3586832a80
Florian Sauter <fn.sauter@gmail.com>
2015-02-17 12:27:38 +0100

    Cleanup of some files.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :trips
+  resources :accounts
 
   get '/dashboard', to: 'dashboard#index', as: 'dashboard'
   get '/sign_up', to: 'accounts#new', as: 'signup'
@@ -15,16 +16,11 @@ Rails.application.routes.draw do
 
   get '/sign_out', to: 'sessions#destroy'
 
-
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :accounts
 
   # Example resource route with options:
   #   resources :products do

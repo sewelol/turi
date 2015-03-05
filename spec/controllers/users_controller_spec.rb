0063f203e78567e1d397e5188736467cce5b6d2e
Runar Flåten <runarflaaten@gmail.com>
2015-03-05 12:36:45 +0100

    Finished user model tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,15 +18,18 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "GET #show" do
+    it "assigns the requested user to user" do
+      get :show, id: user
+      expect(assigns(:user)).to eql(user)
+    end
+
     it "directs to user page when user id exist" do
       get :show, id: user.id
-
       expect(response).to render_template(:show)
     end
 
     it " directs to root page when user id doesn't exist" do
       get :show, id: 202
-
       expect(response).to redirect_to(dashboard_path)
       expect(flash[:alert]).to have_content(I18n.t('user_id_not_found'))
     end

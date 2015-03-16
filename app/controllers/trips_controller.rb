6ae3b6596ebcc5999c16af946eff865adaecddf0
imy005 <ingfvild.kristiane92@gmail.com>
2015-03-16 13:24:57 +0100

    done more with friendship and search

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -40,6 +40,10 @@ class TripsController < ApplicationController
     end
   end
 
+  def search
+    @user, @trips = Trip.search(params[:title_search], params[:location_search], params[:tag_search], params[:date_beg], params[:date_end], params[:name_search])
+  end
+
   def update
     authorize @trip
     if @trip.update(trip_params)
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -43,6 +43,10 @@ class Trip < ActiveRecord::Base
   def self.search(title_search, location_search, tag_search, date_beg, date_end)
     trips = []
 
+    if name_search.present?
+      @user = User.where('name LIKE ?', "%#{name_search}%")
+    end
+
     if title_search.present? || location_search.present?
       trips = Trip.where('title LIKE ? AND (start_loc LIKE ? OR end_loc LIKE ?)', "%#{title_search}%","%#{location_search}%", "%#{location_search}%")
     end

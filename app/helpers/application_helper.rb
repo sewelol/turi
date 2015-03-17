569817d873216bc3ea0092707e2964d1372c1fb9
Florian Sauter <fn.sauter@gmail.com>
2015-03-17 15:25:53 +0100

    Updated participant controller to use TripResourceController and implemented one-liner title helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
 
+  def page_title(title)
+    content_for :page_title do
+      title
+    end
+  end
+
+  def page_title_t(title)
+    page_title t(title)
+  end
+
   def form_group_class(record, attribute)
     error_class = record.errors.include?(attribute) ? 'has-error' : ''
     return "form-group #{error_class}"

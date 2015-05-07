b0c6f5a41ee442fa79a71531571cfcd6d84c027d
Runar Flåten <runarflaaten@gmail.com>
2015-05-07 14:08:51 +0200

    Working on friendship controller test

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,17 +3,26 @@ class FriendshipsController < ApplicationController
   before_action :set_user
 
   def create
-    if params[:request_flag].True
-      @friendship = current_user.friendships.build(:friend_id => @user.id)
-      flash[:notice] = I18n.t 'user_friendship_confirmed'
+    pp "params are: #{params}"
+
+    exist = Friendship.request_exists(params[:user_id], params[:friend_id])
+    if exist
+      @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+      if @friendship.save
+        flash[:notice] = I18n.t 'user_friendship_confirmed'
+      else
+        flash[:alert] = t('user_friendship_not_confirmed')
+      end
     else
-      flash[:alert] = I18n.t 'user_request_not_confirmed'
+      flash[:alert] = I18n.t 'user_friendship_not_confirmed'
     end
     redirect_to user_path @user
   end
 
   def destroy
+    exist = Friendship.friendship_exists(params[:user_id], params[:friend_id])
     @friendship = Friendship.where(friend_id: [current_user, params[:id]]).where(user_id: [current_user, params[:id]]).last
+    @friendship = Friendship.where
     @friendship.destroy
     flash[:notice] = I18n.t 'user_friendship_removed'
     redirect_to current_user
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,22 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => User
+
+  has_many :friendships
+
+  def self.request_exists(user_id, receiver_id)
+    a = Request.exists?(user_id: receiver_id, receiver_id: user_id)
+    b = Request.exists?(user_id: user_id, receiver_id: receiver_id)
+    a || b
+  end
+
+  def self.friendship_exists(user_id, friend_id)
+    a = Friendship.exists?(user_id: friend_id, friend_id: user_id)
+    b = Friendship.exists?(user_id: user_id, friend_id: friend_id)
+    a || b
+  end
+
+  def self.find_friendship(user_id, friend_id)
+    Friendship.where("user_id = ? and friend_id = ? OR")
+  end
 end
